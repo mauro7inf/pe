160b3c0004b4a4e6b62825ffcbc04ce8c0b921ac
Mauro Braunstein <webmaster@offtonic.com>
2017-08-29 19:51:02 -0400

    Solved 229

--- p229.js
+++ p229.js
@@ -2,7 +2,7 @@ let NumberTheory = require('./NumberTheory');
 let BitArray = require('./BitArray');
 
 let N = process.argv[2];
-const steps = 10;
+const steps = 200;
 
 let debugOn = false; // set to true to enable debug
 
@@ -19,29 +19,30 @@ function countInRange(A, B) {
   //let count7 = 0;
 
   for (let a = 1; a*a + 7 <= B; a++) {
-    if (a % 10 === 0) {
-      console.log('[' + A + ' - ' + B + '] a = ' + a);
+    let bStart = a*a < A ? Math.ceil(Math.sqrt((A - a*a)/7)) : 1;
+    if (a % 1000 === 0) {
+      console.log('[' + A + ' - ' + B + '] 7: a = ' + a + ', b = ' + bStart);
     }
-    for (let b = Math.ceil(Math.sqrt((A - a*a)/7)); a*a + 7*b*b <= B; b++) {
+    for (let b = bStart; a*a + 7*b*b <= B; b++) {
       //console.log(NumberTheory.factor(a*a +7*b*b));
       //count7++;
       let number = a*a + 7*b*b;
-      if (/*numbers1[a*a + 2*b*b]*/ number % 4 !== 3 && NumberTheory.canBeSumOfTwoSquares(number)) {
+      numbers7.setBit(number);
+      /*if (number % 4 !== 3 && NumberTheory.canBeSumOfTwoSquares(number)) {
         numbers7.setBit(number);
-      }
+      }*/
     }
   }
-
-  console.log("1 and 7: " + numbers7.countBits() + "\n\n");
 
   let numbers3 = new BitArray();
   //let count3 = 0;
 
   for (let a = 1; a*a + 3 <= B; a++) {
-    if (a % 100 === 0) {
-      console.log('[' + A + ' - ' + B + '] a = ' + a);
+    let bStart = a*a < A ? Math.ceil(Math.sqrt((A - a*a)/3)) : 1;
+    if (a % 1000 === 0) {
+      console.log('[' + A + ' - ' + B + '] 3: a = ' + a + ', b = ' + bStart);
     }
-    for (let b = Math.ceil(Math.sqrt((A - a*a)/3)); a*a + 3*b*b <= B; b++) {
+    for (let b = bStart; a*a + 3*b*b <= B; b++) {
       //console.log(NumberTheory.factor(a*a + 3*b*b));
       //count3++;
       if (numbers7.getBit(a*a + 3*b*b)) {
@@ -50,16 +51,15 @@ function countInRange(A, B) {
     }
   }
 
-  console.log("1, 7, and 3: " + numbers3.countBits() + "\n\n");
-
   let numbers2 = new BitArray();
   //let count2 = 0;
 
   for (let a = 1; a*a + 2 <= B; a++) {
-    if (a % 100 === 0) {
-      console.log('[' + A + ' - ' + B + '] a = ' + a);
+    let bStart = a*a < A ? Math.ceil(Math.sqrt((A - a*a)/2)) : 1;
+    if (a % 1000 === 0) {
+      console.log('[' + A + ' - ' + B + '] 2: a = ' + a + ', b = ' + bStart);
     }
-    for (let b = Math.ceil(Math.sqrt((A - a*a)/2)); a*a + 2*b*b <= B; b++) {
+    for (let b = bStart; a*a + 2*b*b <= B; b++) {
       //console.log(NumberTheory.factor(a*a + 7*b*b));
       //count2++;
       if (numbers3.getBit(a*a + 2*b*b)) {
@@ -68,9 +68,23 @@ function countInRange(A, B) {
     }
   }
 
+  let numbers1 = new BitArray();
+
+  for (let a = 1; a*a + 2 <= B; a++) {
+    let bStart = a*a < A ? Math.ceil(Math.sqrt(A - a*a)) : 1;
+    if (a % 1000 === 0) {
+      console.log('[' + A + ' - ' + B + '] 1: a = ' + a + ', b = ' + bStart);
+    }
+    for (let b = bStart; a*a + b*b <= B; b++) {
+      if (numbers2.getBit(a*a + b*b)) {
+        numbers1.setBit(a*a + b*b);
+      }
+    }
+  }
+
   //console.log("2: " + count2 + "\n\n");
-  let countTemp = numbers2.countBits();
-  console.log("1, 7, 3, and 2: " + countTemp);
+  let countTemp = numbers1.countBits();
+  console.log(countTemp);
   return countTemp;
 }
 
