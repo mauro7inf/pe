85fb5103b7ebb6b4dcb675d777d61a36cf6638ae
mauro-braunstein <mauro.braunstein@jibo.com>
2017-11-14 15:30:54 -0500

    Solved 251!

--- p251.js
+++ p251.js
@@ -2,40 +2,77 @@ let NumberTheory = require('./NumberTheory');
 
 let N = +(process.argv[2]);
 
-let count = 0;
-for (let a = 1;; a++) {
-	let left = 8*a*a*a + 15*a*a + 6*a;
-	let m = (left - 1)/27;
-	let portion = (a + 1.5*Math.pow(2*m, 1/3))/N;
-	if (portion > 1) {
+function curt(x) {
+	return Math.pow(x, 1/3);
+}
+
+let curt2 = curt(2);
+
+let start = 1;
+let end = 2;
+while (1) {
+	let s = 3*end - 1 + 1.5*curt2*curt(end)*curt(end)*curt(8*end - 3);
+	if (s > N) {
 		break;
 	}
-	if ((left - 1) % 27 !== 0) {
-		continue;
+	start = end;
+	end *= 2;
+}
+while (end - start > 0.1) {
+	let mid = (start + end)/2;
+	let s = 3*mid - 1 + 1.5*curt2*curt(mid)*curt(mid)*curt(8*mid - 3);
+	if (s > N) {
+		end = mid;
+	} else {
+		start = mid;
 	}
-	let mFactors = NumberTheory.factor(m);
-	let pFactors = NumberTheory.primeFactors(mFactors);
-	let largestSquare = 1;
-	for (let i = 0; i < pFactors.length; i++) {
-		let p = pFactors[i]
-		let f = mFactors[p];
-		let power = Math.floor(f/2);
-		largestSquare *= Math.pow(p, power);
-	}
-	let squarePairs = NumberTheory.factorPairs(largestSquare);
-	let squares = [];
-	for (let i = 0; i < squarePairs.length; i++) {
-		squares.push(squarePairs[i][0]);
-		if (squarePairs[i][0] !== squarePairs[i][1]) {
-			squares.push(squarePairs[i][1]);
+}
+
+let stepSize = 10000;
+let AStart = 0;
+let count = 0;
+console.log('alpha end: ' + end);
+for (let A = AStart; A < end; A += stepSize) {
+	console.log('alpha = ' + A + ': ' + count);
+	for (let alpha = 1 + A; alpha < end && alpha <= A + stepSize; alpha++) { // a = 3*alpha - 1
+		let a = 3*alpha - 1;
+		let m = 8*alpha - 3;
+		let minSum = a + 1.5*curt2*curt(alpha)*curt(alpha)*curt(m);
+		if (minSum > N + 1) {
+			break;
 		}
-	}
-	for (let i = 0; i < squares.length; i++) {
-		let b = squares[i];
-		let c = m/(b*b);
-		if (a + b + c <= N) {
-			console.log('(' + a + ', ' + b + ', ' + c + ') ' + portion);
-			count++;
+		let mFactors = NumberTheory.factor(m);
+		let pFactors = NumberTheory.primeFactors(mFactors);
+		let largestSquare = {};
+		for (let i = 0; i < pFactors.length; i++) {
+			let p = pFactors[i];
+			if (mFactors[p] >= 2) {
+				largestSquare[p] = Math.floor(mFactors[p]/2);
+			}
+		}
+		let combined = NumberTheory.factorProduct([alpha, largestSquare]);
+		let pairs = NumberTheory.factorPairs(combined);
+		for (let i = 0; i < pairs.length; i++) {
+			let endJ = 2;
+			if (pairs[i][0] === pairs[i][1]) {
+				endJ = 1;
+			}
+			for (let j = 0; j < endJ; j++) {
+				let b = pairs[i][j];
+				let cEstimate = (alpha/b)*(alpha/b)*m;
+				if (a + b + cEstimate > 1.1*N) {
+					continue;
+				}
+				let bFactors = NumberTheory.factor(b); // wastes time
+				let cFactors = NumberTheory.factorProduct([alpha, alpha, m]);
+				cFactors = NumberTheory.factorQuotient(cFactors, bFactors);
+				cFactors = NumberTheory.factorQuotient(cFactors, bFactors);
+				let c = NumberTheory.multiplyFactors(cFactors);
+				if (a + b + c <= N) {
+					count++;
+					//console.log('(' + a + ', ' + b + ', ' + c + ') ' + (minSum/N));
+				}
+			}
 		}
 	}
 }
